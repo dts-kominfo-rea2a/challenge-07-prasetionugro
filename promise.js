82196364e20664476c92c14e8ff567fc90001626
Prasetio Nugroho <prasetio.nugro@gmail.com>
2022-06-10 10:29:34 +0700

    feat: Add promiseOutput function

--- promise.js
+++ promise.js
@@ -1,7 +1,21 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (kataFilter) => {
+  try {
+    const dataTheaterIXX = await promiseTheaterIXX();
+    const dataTheaterVGC = await promiseTheaterVGC();
+    const dataCombine = dataTheaterIXX.concat(dataTheaterVGC);
+
+    const hasil = dataCombine.filter((element) => {
+      return element.hasil === kataFilter;
+    });
+
+    return hasil.length;
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 module.exports = {
   promiseOutput,
